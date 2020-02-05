3f8386e89be07b5467146d0134231601c801e682
Jorge <jorgitoballes@gmail.com>
2020-02-05 16:36:13 +0100

    copias totales averiguadas

--- src/practicarAceptaElReto_repo/problema338.java
+++ src/practicarAceptaElReto_repo/problema338.java
@@ -1,6 +1,8 @@
 package practicarAceptaElReto_repo;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 public class problema338 {
@@ -21,13 +23,28 @@ public class problema338 {
 				examenes.put(i, exam);
 
 			}
+			ArrayList<Integer> memoria = new ArrayList<Integer>();
 
 			for (int i = 1; i <= cant; i++) {
-				examenes.get(i);
+
+				for (int j = 1; j <= mem; j++) {
+					memoria.add(examenes.get(j));
+
+				}
 
 			}
 
+			// Averiguar copias totales
+			LinkedHashSet<Integer> copiastot = new LinkedHashSet<Integer>();
+			for (int i = 1; i <= cant; i++) {
+				copiastot.add(examenes.get(i));
+			}
+			int totalcopias = examenes.size()-copiastot.size();
+			System.out.println(totalcopias);
+			//copias totales terminado
+
 			System.out.println(examenes.toString());
+			System.out.println(memoria.toString());
 
 		}
 
